f46cf1077a2d4073e14be4f854fb50957460f1b1
Jay Miller <kjaymiller@gmail.com>
2022-09-23 12:50:11 -0400

    update github.py

--- src/github.py
+++ src/github.py
@@ -1,62 +1,25 @@
 import os
 import re
 from collections import defaultdict, namedtuple
-from typing import Any, Generator
-
+from typing import Any, Sequence, Generator
+import dataclasses
 import httpx
 from markdown_it import MarkdownIt
 from markdown_it.tree import SyntaxTreeNode
 
-Issue = namedtuple("Issue", "metadata body")
 
-
-class Episode:
-    def __init__(self, episode_id: int, issue_fields: list):
-        self.episode_id = episode_id
-        self.raw = get_from_github(self.episode_id)
-        self.body = parse_issue_markdown(self.raw["body"])
-
-        for key, value in self.body.items():
-            setattr(self, key, value)
-
-        for field in issue_fields:
-            setattr(self, field, list(get_issues(get_content_issues(self.body, field))))
-
-    @property
-    def title(self) -> str:
-        return self.raw["title"]
-
-    @property
-    def created_at(self) -> str:
-        return self.raw["created_at"]
-
-
-def get_from_github(issue_id: str | int) -> dict[str, str]:
+def get_content_issues(
+    body: dict[str, Any], 
+    issues_tag: str,
+)  -> list[int]:
     """
-    Returns the issue with the given id.
-    The issue_id must be a valid integer.
+    Returns Issues Passed in sections of the issue body
     """
-
-    url = f"https://api.github.com/repos/kjaymiller/Python-Community-News/issues/{str(issue_id)}"
-    headers = {
-        "Authorization": f"Bearer {os.environ['GITHUB_API_TOKEN']}",
-    }
-    request = httpx.get(url, headers=headers)
-    if request.status_code not in [200, 201]:
-        raise ConnectionRefusedError(f"Unable to connect: {request.json()}")
-    return request.json()
+    issues = getattr(body, issues_tag, "")
+    return [int(n) for n in re.findall(r"\d+", issues)]
 
 
-def get_issues(issues) -> Generator[dict[str, str], None, None]:
-    """Returns the issues filed in the last week"""
-    for issue in issues:
-        issue_content = get_from_github(issue)
-        body = parse_issue_markdown(issue_content["body"])
-        i = Issue(metadata=issue_content, body=body)
-        yield i
-
-
-def parse_issue_markdown(text) -> dict:
+def parse_issue_markdown(text) -> dict[str, str]:
     """Use markdownit to split at section headings"""
     md = MarkdownIt("zero", {"maxNesting": 1})
     md.enable(["heading", "paragraph"])
@@ -71,17 +34,63 @@ def parse_issue_markdown(text) -> dict:
         else:
             issue_object[issue_key].append(n.children[0].content)
 
-    for key, value in issue_object.items():
-        issue_object[key] = "\n".join(value)
+    return {key: "\n".join(value) for key, value in issue_object.items()}
+        
 
-    return issue_object
+@dataclasses.dataclass
+class Repo:
+    owner: str
+    repo: str
+
+    @property
+    def url(self):
+        return f"https://api.github.com/repos/{self.owner}/{self.repo}"
 
 
-def get_content_issues(
-    body: dict[str, Any], issues_tag: str
-) -> Generator[dict[str, str], None, None]:
+def get_from_github(Repo: Repo, issue_id: int) -> dict[str, str]:
     """
-    Returns Issues Passed in sections of the issue body
+    Returns the issue with the given id.
+    The issue_id must be a valid integer.
     """
-    issues = re.findall(r"\d+", body[issues_tag])
-    return issues
+
+    url = f"https://api.github.com/repos/{Repo.owner}/{Repo.repo}/issues/{str(issue_id)}"
+    headers = {
+        "Authorization": f"Bearer {os.environ['GITHUB_API_TOKEN']}",
+    }
+    request = httpx.get(url, headers=headers)
+
+    if request.status_code not in [200, 201]:
+        raise ConnectionRefusedError(f"Unable to connect: {request.json()}")
+
+    return request.json()
+
+Issue = namedtuple("Issue", "metadata body") 
+
+def get_issues(issues: Sequence, Repo:Repo) -> Generator[Issue, None, None]:
+    """Gets issues from the section"""
+    for issue in issues:
+        issue_content = get_from_github(Repo, issue)
+        body = parse_issue_markdown(issue_content["body"])
+        i = Issue(metadata=issue_content, body=body)
+        yield i
+
+
+class Episode:
+    def __init__(self, episode_id: int, issue_fields: list, Repo: Repo):
+        self.episode_id = episode_id
+        self.raw = get_from_github(issue_id=self.episode_id, Repo=Repo)
+        self.body = parse_issue_markdown(self.raw["body"])
+
+        for key, value in self.body.items():
+            setattr(self, key, value)
+
+        for field in issue_fields:
+            setattr(self, field, list(get_issues(issues=get_content_issues(self.body, field), Repo=Repo)))
+
+    @property
+    def title(self) -> str:
+        return self.raw["title"]
+
+    @property
+    def created_at(self) -> str:
+        return self.raw["created_at"]
