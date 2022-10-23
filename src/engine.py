53ab0145d41b47c15d72a9848bde919c66cec0d3
Jay <kjaymiller@gmail.com>
2022-10-23 13:34:02 -0700

    updates template to use gh-issues

--- src/engine.py
+++ src/engine.py
@@ -1,3 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
+from dateutil import parser
 
 engine = Environment(loader=FileSystemLoader(["templates", "automation/templates"]))
+
+def datetime_format(value, format="%d-%m-%y"):
+    return parser.isoparse(value).strftime(format)
+
+engine.filters["datetime_format"] = datetime_format
